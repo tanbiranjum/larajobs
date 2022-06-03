ad210b71f55f5bcec424eee7d21365bef9fe4c97
tanbiranjum <tanbiranjum@hotmail.com>
2022-06-04 02:10:03 +0600

    usercontroller added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use PhpParser\Node\Expr\List_;
 
 class ListingController extends Controller
 {
@@ -46,5 +47,35 @@ class ListingController extends Controller
 
         Listing::create($formFields);
         return redirect('/')->with('message', 'Listing created successfully');
+    }
+
+    public function edit(Listing $listing)
+    {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    public function update(Request $request, Listing $listing)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => 'required',
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+        return back()->with('message', 'Listing updated successfully');
+    }
+
+    public function destroy(Listing $listing)
+    {
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -21,7 +22,20 @@ Route::get('/', [ListingController::class, 'index']);
 // Show create form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
+// Create listing
 Route::post('/listings', [ListingController::class, 'store']);
+
+// Update listing form
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register create form
+Route::get('/register', [UserController::class, 'create']);
